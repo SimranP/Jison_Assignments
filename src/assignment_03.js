2e61afdfbc6f3d165a46b7e9411db759dbe38db2
Simran pal <simranp@thoughtworks.com>
2016-11-23 13:25:22 +0530

    Completed upto assignment_03

--- src/assignment_03.js
+++ src/assignment_03.js
@@ -1,14 +1,15 @@
 var fs = require("fs");
 var jison = require("jison");
-var data = process.argv[2];
-var represent =  require("./assignment_01.js");
 
-var bnf = fs.readFileSync("parse_tree.jison", "utf8");
+var bnf = fs.readFileSync(process.cwd()+"/lib/parse_tree.jison", "utf8");
 var parser = new jison.Parser(bnf);
-var trees = parser.parse(data);
 
-var result = trees.map(function(tree) {
-	return tree.evaluate();
-})
+var evaluate = function(expression) {
+	var trees = parser.parse(expression);
+	var results = trees.map(function(tree) {
+		return tree.evaluate();
+	})
+	return results[results.length-1];
+};
 
-console.log(result);
+module.exports = evaluate;
